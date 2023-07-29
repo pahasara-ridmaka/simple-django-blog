508584b75f429633b33f264de4758779c30d35d9
codemonke-jpg <pridmaka@gmail.com>
2023-07-29 18:20:38 +0530

    Added Options: create, edit and delete posts

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -7,3 +8,7 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+    
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -14,3 +16,24 @@ class BlogDetailView(DetailView):
     model = Post
 
     template_name = 'post_detail_view.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    
+    model = Post
+    template_name = 'post_delete.html'
+
+    success_url = reverse_lazy('home')
+
+
+    
